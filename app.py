167e7b8b429831d0abfe22324b94fb2e61b11335
thinh-huynh-re <thinh.researcher@gmail.com>
2023-07-14 08:32:37 +0700

    [Update] app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from argparser import ArgParser
 from inference import Detector, count_parameters, load_model_for_inference
 from glob import glob
 
+st.title("TransDETR")
+st.image("pipeline.png")
+
 
 def video2frames(video_path: str, dest: str):
     # print("video_path", video_path, "dest", dest)
@@ -38,54 +41,65 @@ args.show = True
 if args.output_dir:
     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 
-with st.spinner("Loading model ..."):
-    model = load_model_for_inference(args)
-    st.text(f"Number of parameters {count_parameters(model)}")
+mode = st.radio("Mode", ["Inference", "Gallary"])
 
-f = st.file_uploader("Upload video", type=["mp4"])
-if f is not None:
-    video_path = f"data/videos/upload/{f.name}"
-    with open(video_path, "wb") as tfile:
-        tfile.write(f.read())
+if mode == "Inference":
+    with st.spinner("Loading model ..."):
+        model = load_model_for_inference(args)
+        st.text(f"Number of parameters {count_parameters(model)}")
+    f = st.file_uploader(
+        "Upload video",
+        type=["mp4"],
+    )
+    if f is not None:
+        video_path = f"data/videos/upload/{f.name}"
+        with open(video_path, "wb") as tfile:
+            tfile.write(f.read())
 
+        st.video(video_path)
+        fname_wo_ext = fname_without_ext(video_path)
+        with st.spinner("1. Extract frames from video ..."):
+            video2frames(
+                video_path,
+                os.path.join("data/frames/upload", fname_wo_ext),
+            )
+
+        with st.spinner("2. Inference ..."):
+            det = Detector(args, model=model, video_name=fname_wo_ext)
+            time_cost = det.detect(vis=args.show)
+
+        with st.spinner("3. Make video from frames"):
+            dirpath = f"data/outputs/upload/results/{fname_wo_ext}"
+            num_frames = len(glob(f"{dirpath}/*.jpg"))
+            os.makedirs("data/outputs/upload/videos", exist_ok=True)
+            if num_frames > 0:
+                first_frame = Image.open(f"{dirpath}/0.jpg")
+                print(first_frame.size)
+                vf = cv2.VideoCapture(video_path)
+                fps = vf.get(cv2.CAP_PROP_FPS)
+                print("fps", fps)
+                out_path = f"data/outputs/upload/videos/{fname_wo_ext}.avi"
+                final_out_path = f"data/outputs/upload/videos/{fname_wo_ext}.mp4"
+                out = cv2.VideoWriter(
+                    out_path,
+                    cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+                    fps,
+                    first_frame.size,
+                )
+                for i in stqdm(range(num_frames)):
+                    out.write(cv2.imread(f"{dirpath}/{i}.jpg"))
+                out.release()
+                vf.release()
+
+                os.system(
+                    f'ffmpeg -y -i "{out_path}" -vcodec libx264 "{final_out_path}" -hide_banner -loglevel error'
+                )
+                os.system(f"rm {out_path}")
+
+        st.video(final_out_path)
+
+else:
+    video_paths = list(glob("data/outputs/upload/videos/*.mp4"))
+    st.text(f"Number of videos: {len(video_paths)}")
+    video_path = st.selectbox("Choose a video", video_paths)
     st.video(video_path)
-    fname_wo_ext = fname_without_ext(video_path)
-    with st.spinner("1. Extract frames from video ..."):
-        video2frames(
-            video_path,
-            os.path.join("data/frames/upload", fname_wo_ext),
-        )
-
-    with st.spinner("2. Inference ..."):
-        det = Detector(args, model=model, video_name=fname_wo_ext)
-        time_cost = det.detect(vis=args.show)
-
-    with st.spinner("3. Make video from frames"):
-        dirpath = f"data/outputs/upload/results/{fname_wo_ext}"
-        num_frames = len(glob(f"{dirpath}/*.jpg"))
-        os.makedirs("data/outputs/upload/videos", exist_ok=True)
-        if num_frames > 0:
-            first_frame = Image.open(f"{dirpath}/0.jpg")
-            print(first_frame.size)
-            vf = cv2.VideoCapture(video_path)
-            fps = vf.get(cv2.CAP_PROP_FPS)
-            print("fps", fps)
-            out_path = f"data/outputs/upload/videos/{fname_wo_ext}.avi"
-            final_out_path = f"data/outputs/upload/videos/{fname_wo_ext}.mp4"
-            out = cv2.VideoWriter(
-                out_path,
-                cv2.VideoWriter_fourcc("M", "J", "P", "G"),
-                fps,
-                first_frame.size,
-            )
-            for i in stqdm(range(num_frames)):
-                out.write(cv2.imread(f"{dirpath}/{i}.jpg"))
-            out.release()
-            vf.release()
-
-            os.system(
-                f'ffmpeg -y -i "{out_path}" -vcodec libx264 "{final_out_path}" -hide_banner -loglevel error'
-            )
-            os.system(f"rm {out_path}")
-
-    st.video(final_out_path)
